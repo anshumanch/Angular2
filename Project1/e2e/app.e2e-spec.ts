19eaefb34be8f1848e01a7cb145d7ce6097f8ef0
anshumanch <anshuman.chakravarty@gmail.com>
2017-03-19 07:38:48 -0400

    Tried a few test cases

--- Project1/e2e/app.e2e-spec.ts
+++ Project1/e2e/app.e2e-spec.ts
@@ -11,4 +11,26 @@ describe('project1 App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('app works!');
   });
+
+  it('should display the table', () => {
+    page.navigateTo();
+    expect(page.getTableText()).toEqual(`
+    <table>
+      <tr>
+        <td>Anshuman</td>
+        <td>POC</td>
+        <td>10</td>
+        <td>Arpita</td>
+        <td>Development</td>
+        <td>20</td>
+        <td>Aryan</td>
+        <td>Testing</td>
+        <td>15</td>
+        <td>Manjula</td>
+        <td>Deployment</td>
+        <td>10</td>
+      </tr>
+    </table>
+    `);
+  });
 });
--- Project1/e2e/app.po.ts
+++ Project1/e2e/app.po.ts
@@ -8,4 +8,8 @@ export class Project1Page {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getTableText() {
+    return element(by.css('emp')).getText();
+  }
 }
